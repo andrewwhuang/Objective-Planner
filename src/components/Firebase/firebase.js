5b8cb91b16d6c75583f0de5f2df6729b187d474c
Matthew Yen <mgyen@shaw.ca>
2021-01-09 15:22:29 -0800

    added firebase auth functions

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,6 +1,7 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 
+
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -13,8 +14,22 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
-
     this.auth = app.auth();
+  }
+  createUser = (email, password) => {
+    this.auth.createUserWithEmailAndPassword(email, password);
+  }
+  
+  signUserIn = (email, password) => {
+    this.auth.signInWithEmailAndPassword(email, password);
+  }
+  
+  signOut = () => {
+    this.auth.signOut();
+  }
+
+  resetPassword = (email) => {
+    this.auth.sendPasswordResetEmail(email);
   }
 }
 
