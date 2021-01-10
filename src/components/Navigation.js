e3a30a8564cb219eadeaf894b367fa08604d7001
Matthew Yen <mgyen@shaw.ca>
2021-01-09 20:43:36 -0800

    style changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -23,10 +23,10 @@ const NavigationAuth = () => (
 );
 
 const NavigationNonAuth = () => (
-    <div className ="navbar-light bg-dark" style = {{paddingRight: '15px'}}>
+    <div className ="navbar-light bg-dark" style = {{paddingRight: '10px', paddingBottom: '1px'}}>
         <h2 className="text-white" style = {{textAlign: 'center'}}>Budget Tracker</h2>
         <Link to={ROUTES.SIGN_IN}><button className="btn btn-dark" style = {{float: 'right', width: '100px', margin:'5px'}}>Sign In</button></Link>
-        <Link to={ROUTES.SIGN_UP}><button className="btn btn-dark" style = {{float: 'right', width: '100px', margin: '5px'}}>Sign Up</button></Link>
+        <Link to={ROUTES.SIGN_UP}><button className="btn btn-dark" style = {{float: 'right', width: '100px', margin:'5px'}}>Sign Up</button></Link>
     </div>
 );
  
