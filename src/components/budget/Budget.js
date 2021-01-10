3f208464d34aa5ec2d1ecef7e0909b6bee36110d
Matthew Yen <mgyen@shaw.ca>
2021-01-10 02:39:14 -0800

    merge conflict

--- src/components/budget/Budget.js
+++ src/components/budget/Budget.js
@@ -9,16 +9,10 @@ import SaveButton from '../SaveButton'
 
 const Budget = () => {
     return (
-<<<<<<< HEAD
         <div className="row" style = {{paddingTop: '10px', paddingBottom: '10px'}}>
             <div className ="col-lg-4" >
                 <InputBudgetFB />
                 <div style = {{paddingBottom: '10px'}}></div>
-=======
-        <div className="row">
-            <div className ="col-lg-4">
-                <InputBudget />
->>>>>>> e4674ec87cac5f7feb956f3867c7ec7c7fd86f04
                 <InputExpense />
                 <div style = {{paddingBottom: '10px'}}></div>
                 <InputGoal />
